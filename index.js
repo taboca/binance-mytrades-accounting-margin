212b53954bffb4fc0e6c42e5482cc8b2e5ce3219
Marcio Galli <taboca@gmail.com>
2021-04-09 18:24:05 -0300

    Patch to use the local node-binance-api

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const Binance = require('node-binance-api');
+
+/* 
+// This is the margin example, 
+// See this 
+a15-010 accounts: Binance API - trying to understand how to get the account trades data — e2 — 0421981a-d179-4a27-a0d1-fe1280b5e1e5 Friday, April 9⋅2:00 – 5:30pm
+*/
+const Binance = require('./node-binance-api');
+
+// This is the spot example
+//const Binance = require('node-binance-api');
 const Keys    = require('./config-REMOVETHIS.js');
 
 const binance = new Binance().options(Keys);
 
+// Trying to set date rande 
+
+let initialDate = new Date("Mon Apr 9 2021 13:00:00 GMT-0300");
+let lastDate    = new Date("Mon Apr 9 2021 14:15:00 GMT-0300");
+
 // This example shows how to pass options
 
 let options = { 
-  limit: 2 // Limit results to two entries
+  limit       : 10, 
+  isIsolated  : true, 
+  startTime   : initialDate.getTime(), 
+  endTime     : lastDate.getTime(), 
 };
 
-binance.trades("BTCBRL", (error, trades, symbol) => {  
+binance.trades("ETHUSDT", (error, trades, symbol) => {  
   for(let k in trades) { 
     let trade = trades[k];
     let tradeDateTime = new Date(trade.time);
-    console.log(`Trade event --------------------------------`);
+    console.log(`Trade event ${k} ------------------------------`);
     console.log(`time             : ${tradeDateTime}`);
     console.log(`symbol           : ${trade.symbol}`);
     console.log(`price            : ${trade.price}`);
